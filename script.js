4936fdd1ea80e4326ccd909c4ecb5742a2e7ccf0
priyanshuxch <priyanshuxch@gmail.com>
2025-07-28 19:19:54 +0530

    Make GET request using another API and edit css

--- script.js
+++ script.js
@@ -35,22 +35,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function displayUserData(parsedData) {
     // total ques
-    const totalQues = parsedData.data.allQuestionsCount[0].count;
-    const totalEasyQues = parsedData.data.allQuestionsCount[1].count;
-    const totalMediumQues = parsedData.data.allQuestionsCount[2].count;
-    const totalHardQues = parsedData.data.allQuestionsCount[3].count;
+    const totalQues = parsedData.totalQuestions;
+    const totalEasyQues = parsedData.totalEasy;
+    const totalMediumQues = parsedData.totalMedium;
+    const totalHardQues = parsedData.totalHard;
 
     // total solved ques
-    const totalSolvedQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
-    const totalEasySolvedQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
-    const totalMediumSolvedQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
-    const totalHardSolvedQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+    const totalSolvedQues = parsedData.totalSolved;
+    const totalEasySolvedQues = parsedData.easySolved;
+    const totalMediumSolvedQues = parsedData.mediumSolved;
+    const totalHardSolvedQues = parsedData.hardSolved;
 
     // populate data in progress circles
     updateProgress(totalEasySolvedQues, totalEasyQues, easyLabel, easyProgressCircle);
     updateProgress(totalMediumSolvedQues, totalMediumQues, mediumLabel, mediumProgressCircle);
     updateProgress(totalHardSolvedQues, totalHardQues, hardLabel, hardProgressCircle);
+
+    const cardData = [
+      {label:"Acceptance Rate", value:parsedData.acceptanceRate},
+      {label:"Ranking", value:parsedData.ranking},
+      {label:"Contribution Points", value:parsedData.contributionPoints},
+      {label:"Reputation", value:parsedData.reputation},
+    ];
+
+    let cardsHTML = '';
+    cardData.map((card) => {
+      const html = `
+        <div class="card">
+          <h4>${card.label}</h4>
+          <p>${card.value}</p>
+        </div>
+      `;
+      cardsHTML += html;
+    });
     
+    cardStatsContainer.innerHTML = cardsHTML;
   }
 
   async function fetchUserDetails(username) {
@@ -58,23 +77,8 @@ document.addEventListener("DOMContentLoaded", function () {
     searchBtn.disabled = true;
 
     try {
-      const proxyUrl = `https://cors-anywhere.herokuapp.com/`; 
-      const targetUrl = `https://leetcode.com/graphql/`;
-      const myHeaders = new Headers();
-      myHeaders.append("content-type", "application/json");
-
-      const graphql = JSON.stringify({
-          query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
-          variables: { "username": `${username}` }
-      })
-      const requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: graphql,
-          redirect: "follow"
-      };
-
-      const response = await fetch(proxyUrl+ targetUrl, requestOptions);
+      const url = `https://leetcode-stats-api.herokuapp.com/${username}`;
+      const response = await fetch(url);
 
       if(!response.ok) {
         throw new Error("Unable to fetch the user details");
